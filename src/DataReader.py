f26d02858fac6706cfd297c8b71fa600d7c0a496
kiennguyen94 <hakien94@gmail.com>
2017-03-03 18:20:56 -0500

    Data reader class

--- src/DataReader.py
+++ src/DataReader.py
@@ -11,6 +11,7 @@ class DataReader(object):
     map_file: text file that defines the dictionary structure
     data_file: json data file
     Assume responsibility of caller to specify options
+    If no option object is specified, then extract_features returns raw matrix.
     """
 
     def __init__(self, map_file, data_file, opt=None):
@@ -18,23 +19,36 @@ class DataReader(object):
         self.y = np.zeros((1, 1))  # labels column vector
         self.map_file = map_file
         self.data_file = data_file
-        if opt is None:
-            self.opt = DataParserOpt()
-        # if self.opt.only_left is True:
-        #     assert self.opt.only_right is False
-        # if self.opt.only_right:
-        #     assert self.opt.only_left is False
-        # assert type(opt) is DataParserOpt
+        self.opt = opt
+
+    def _filter_(self, nnz_idx, A, y):
+        """
+        Internal function, not to be used externally.
+        Called when self.opt is not None
+        indx: index of valid frames.
+        """
+        # number of features per hand
+        HAND_LENGTH = 186
+        if self.opt is None:
+            return A, y
+
+        if self.opt.no_empty:
+            # pass
+            A = A[nnz_idx, :]
+            y = [y[i] for i in nnz_idx]
+        if self.opt.only_left:
+            A = A[:, :HAND_LENGTH]
+        if self.opt.only_right:
+            A = A[:, HAND_LENGTH:]
+        return A, y
 
     def extract_features(self):
         """
-        Parse the JSON file and and place all samples as rows into a Numpy array
+        Parse the JSON file and and place all samples as rows into a np array.
         Return feature array with corresponding label array
         """
-
         # place features names in F
         with open(self.map_file) as f:
-            # if self.opt.only_left:
             feature_list = f.read().splitlines()
 
         # open JSON data in nested dictionary, D
@@ -44,49 +58,28 @@ class DataReader(object):
         # get number of frames and features
         num_frames = len(D)
         num_feats = len(feature_list)
-        print num_frames
-        # print feature_list[0].split('.')
+
         # numpy data array
         A = np.zeros((num_frames, num_feats))
         # keep track of non zero frames
         nnz_idx = []
-        # y = np.zeros((num_frames,))
         y = ['' for i in xrange(num_frames)]
-        # A = np.zeros([1, num_feats])
-        # y = []
+
         for frame_idx in xrange(num_frames):
-            # y[frame_idx] = D[frame_idx]['timestamp']
-            # y[frame_idx] = D[frame_idx]['label']
             if D[frame_idx]['num_hands'] != 0 and D[frame_idx]['num_fingers'] % 5 == 0:
                 nnz_idx.append(frame_idx)
             for feat_idx, feat in enumerate(feature_list):
+                if feat_idx in range(186, 189):
+                    print feat
                 feat_keys = feat.split('.')[1:]
-                # if self.opt.no_empty and D[frame_idx]['num_hands'] == 0:
-                #     continue
-                # if there is atleast 1 hand -> non empty frame, add to list
-
-                # y.append(D[frame_idx]['label'])
                 try:
                     val = reduce(operator.getitem, feat_keys, D[frame_idx])
-                    # A = np.vstack((A, val))
                     A[frame_idx, feat_idx] = val
                     y[frame_idx] = D[frame_idx]['label']
-                    # pdb.set_trace()
-                    # y.append(D[frame_idx]['label'])
                 except KeyError, e:
-                    # print('KeyError: {} in frame {}'.format(e, frame))
                     pass
-        if self.opt.no_empty:
-            # pass
-            print len(nnz_idx)
-            A = A[nnz_idx, :]
-            y = [y[i] for i in nnz_idx]
-        if self.opt.only_left:
-            A = A[:, :186]
-        if self.opt.only_right:
-            A = A[:, 186:]
-        # if self.opt.r
-        return A, y
+            break
+        return self._filter_(nnz_idx, A, y)
 
 
 class DataParserOpt(object):
@@ -100,6 +93,9 @@ class DataParserOpt(object):
     both: get both hands, default to false
     TODO: MORE OPTIONS HERE
     """
+
+    # TODO: design decision... Could've been a dictionary. Not necessary a
+    # class.
 
     def __init__(self, no_empty=True, only_right=True, only_left=False, both=False):
         self.no_empty = no_empty

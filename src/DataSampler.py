21c662c99b032bc3155ea199caf5ca6c710316c5
parth <prparikh02@gmail.com>
2017-03-19 01:22:50 -0400

    Refactored DataSampler to interface better with other parts of the pipeline

--- src/DataSampler.py
+++ src/DataSampler.py
@@ -1,62 +1,83 @@
 from __future__ import division
+import Constants
 import numpy as np
-import json
-import math
+from types import *
 
 
 class DataSampler(object):
 
-    def __init__(self, data, method, *method_args):
-        self.data = data  # np matrix; m samples, n features
-        self.data_length = data.shape[0]
-        self.method = method
-        self.method_args = method_args
+    def __init__(self, data, labels, method, **kwargs):
+        self.data = data
+        self.labels = labels
+        self.method, self.method_args = self._resolve_method_(method, kwargs)
 
-        if self.method == 'uniform_sampling':
-            self.__class__.__call__ = self.uniform_sampling(
-                *(self.method_args))
-        elif self.method == 'random_sampling':
-            self.__class__.__call__ = self.random_sampling(*(self.method_args))
+    def _resolve_method_(self, method, method_args):
+        if method == 'uniform_rate':
+            if 'k' in method_args:
+                return self._uniform_rate_, method_args['k']
+            return self._uniform_rate_, None
+        elif method == 'uniform_number':
+            if 'N' in method_args:
+                return self._uniform_number_, method_args['N']
+            return self._uniform_number_, None
+        elif method == 'simple_random':
+            if 'N' in method_args:
+                return self._simple_random_, method_args['N']
+            return self._simple_random_, None
+        elif method == 'prob':
+            if 'p' in method_args:
+                return self._prob_, method_args['p']
+            return self._prob_, None
+        raise ValueError('No matching method found')
 
-    def uniform_sampling(self, method='rate', value=None):
+    def _uniform_rate_(self, k=None):
+        if not k or k > self.data.shape[0]:
+            return self._uniform_number_()
+        assert type(k) is IntType, 'k is not an integer: {}'.format(k)
+        return self.data[::k], self.labels[::k]
+
+    def _uniform_number_(self, N=None):
         """
-        Inputs: data - an m, n dimension matrix (m samples, n features)
-                method - a string param which specifies the type of uniform sampling
-                       - 'rate' => uniform sampling rate (every k frames)
-                       - 'num_frames' => determine a uniform rate 'k' as length_of_data/num_frames
-                value  - integer value the particular method utilizes
-
-        Outputs: data matrix (m', n), where m' is the new number of data points
+        Discouraged from because if N does not divide number of samples
+            nicely, the sampling is heavily skewed
         """
+        num_frames = self.data.shape[0]
+        if not N:
+            k = int(np.floor(
+                Constants.DEFAULT_SAMPLE_PERCENTAGE * num_frames
+            ))
+            if k == 0:
+                k = 1
+        elif N > num_frames:
+            k = 1
+        else:
+            assert type(N) is IntType, 'N is not an integer: {}'.format(N)
+            k = int(np.floor(num_frames / N))
 
-        if value is None or not isinstance(value, (int, long)):
-            print "The rate and number of frames should be a positive integer!"
-            return None
+        # ensure that at most N samples are taken if N does not divide nicely
+        return self.data[::k][:N], self.labels[::k][:N]
 
-        if value > self.data_length:
-            value = self.data_length
+    def _simple_random_(self, N=None):
+        num_frames = self.data.shape[0]
+        if not N or N > num_frames:
+            N = int(np.floor(
+                Constants.DEFAULT_SAMPLE_PERCENTAGE * num_frames
+            ))
+        assert type(N) is IntType, 'N is not an integer: {}'.format(N)
 
-        if method == 'rate' and value > 0 and isinstance(value, (int, long)):
-            return self.data[0::value]
-        elif method == 'num_frames' and value > 0 and isinstance(value, (int, long)):
-            new_data = self.data[0::int(math.floor(self.data_length / value))]
-            if self.data_length > value:
-                new_data = new_data[0:value]
-            return new_data
+        idx = np.sort(np.random.choice(xrange(num_frames), N, replace=False))
+        return self.data[idx], self.labels[idx]
 
-    def random_sampling(self, num_frames=None):
-        """
-        Inputs: data - an m, n dimension matrix (m samples, n features)
-                num_frames - if num_frames < m, num_frames randomly sampled frames are complied without replacement
+    def _prob_(self, p=None):
+        if not p:
+            p = Constants.DEFAULT_SAMPLE_PERCENTAGE
+        assert 0.0 < p < 1.0, 'p must be in between [0.0, 1.0]: {}'.format(p)
+        num_frames = self.data.shape[0]
+        while True:
+            idx = [i for i in xrange(num_frames) if np.random.uniform() < p]
+            if idx:
+                break
+        return self.data[idx], self.labels[idx]
 
-        Outputs: data matrix (m', n), where m' is the new number of data points
-        """
-
-        if num_frames is None or not isinstance(num_frames, (int, long)):
-            print "The number of frames should be a positive integer!"
-            return None
-
-        if num_frames > self.data_length:
-            num_frames = self.data_length
-
-        return self.data[np.random.choice(self.data_length, num_frames, replace=False), :]
+    def sample(self):
+        return self.method(self.method_args)
